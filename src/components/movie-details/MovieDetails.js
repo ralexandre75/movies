664da65811c8f4406ddb7915d5d4050965d5c14e
ralexandre75 <rene.alexandre@grimoire-design.fr>
2019-10-07 18:04:42 +0200

    component creation

---
+++ src/components/movie-details/MovieDetails.js
@@ -0,0 +1,8 @@
+import React, {Component } from 'react';
+
+export default class MovieDetail extends Component {
+    
+    render() {
+        return <div className="w-25">Movie Details</div>
+    }
+}
---
+++ src/components/movie-list/MovieList.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default class MovieList extends Component {
+
+    render() {
+        return <div className="w-75">Movie List</div>
+    }
+}
